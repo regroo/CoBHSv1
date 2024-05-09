490fe1c48de321030adb4aa8d5b91ece4f38526d
Reggie3 <regroo2@gmail.com>
2024-05-09 14:08:59 +0000

    cheeky commit

--- lib/student/mainHPage.dart
+++ lib/student/mainHPage.dart
@@ -16,62 +16,79 @@ class MainHPage extends StatelessWidget {
       drawer: Drawer(
         child: Container(
           color: darkBlue, // Setting solid color background for the drawer
-          child: ListView(
-            children: [
-              DrawerHeader(
-                child: Text("Student Menu",
-                    style: TextStyle(color: Colors.white)), // Making text white
-              ),
-              ListTile(
-                title: Text("View Timetable",
-                    style: TextStyle(color: Colors.white)), // Making text white
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ViewBookings()),
-                  );
-                },
-              ),
-              ListTile(
-                title: Text("Cancel Booking",
-                    style: TextStyle(color: Colors.white)), // Making text white
-                onTap: () {
-                  // Navigate to the cancel booking page
-                },
-              ),
-              ListTile(
-                title: Text("View Payslip",
-                    style: TextStyle(color: Colors.white)), // Making text white
-                onTap: () {
-                  // Navigate to the create payslip page
-                },
-              ),
-              ListTile(
-                title: Text("View Feedback",
-                    style: TextStyle(color: Colors.white)), // Making text white
-                onTap: () {
-                  // Navigate to the change details page
-                },
-              ),
-              ListTile(
-                title: Text("Switch Accounts",
-                    style: TextStyle(color: Colors.white)), // Making text white
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                },
-              ),
-            ],
+          child: SingleChildScrollView(
+            // Wrapping ListView with SingleChildScrollView
+            child: Column(
+              children: [
+                DrawerHeader(
+                  child: Text(
+                    "Student Menu",
+                    style: TextStyle(color: Colors.white), // Making text white
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "View Timetable",
+                    style: TextStyle(color: Colors.white), // Making text white
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ViewBookings()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    "Cancel Booking",
+                    style: TextStyle(color: Colors.white), // Making text white
+                  ),
+                  onTap: () {
+                    // Navigate to the cancel booking page
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    "View Payslip",
+                    style: TextStyle(color: Colors.white), // Making text white
+                  ),
+                  onTap: () {
+                    // Navigate to the create payslip page
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    "View Feedback",
+                    style: TextStyle(color: Colors.white), // Making text white
+                  ),
+                  onTap: () {
+                    // Navigate to the change details page
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    "Switch Accounts",
+                    style: TextStyle(color: Colors.white), // Making text white
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
       body: Container(
         color: darkBlue, // Setting solid color background to dark blue
         child: Center(
-          child: Text("Main Admin Page Content",
-              style: TextStyle(color: Colors.white)), // Making text white
+          child: Text(
+            "Main Admin Page Content",
+            style: TextStyle(color: Colors.white), // Making text white
+          ),
         ),
       ),
     );
